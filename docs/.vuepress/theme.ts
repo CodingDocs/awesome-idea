0fa79b0aaabb2e001a8ae4100d9a7fdf658f297c
Guide <koushuangbwcx@163.com>
2026-01-03 10:32:15 +0800

    vuepress版本升级

--- docs/.vuepress/theme.ts
+++ docs/.vuepress/theme.ts
@@ -1,3 +1,4 @@
+import { getDirname, path } from "vuepress/utils";
 import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
@@ -9,36 +10,59 @@ export default hopeTheme({
   hostname: "https://codingdocs.github.io",
   repo: "CodingDocs/awesome-idea",
   docsDir: "docs",
-  iconAssets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
   pure: true,
   breadcrumb: false,
   navbar,
   sidebar,
-
-  pageInfo: ["Author", "Category", "Tag", "Date", "Original", "Word"],
-
-  blog: {
-    intro: "https://javaguide.cn/about-the-author/",
-    sidebarDisplay: "mobile",
-    medias: {
-      Zhihu: "https://www.zhihu.com/people/javaguide",
-      Github: "https://github.com/Snailclimb",
-      Gitee: "https://gitee.com/SnailClimb",
-    },
-  },
-
   footer:
     '主题: <a href="https://theme-hope.vuejs.press/zh/">VuePress Theme Hope</a></br><a href="https://beian.miit.gov.cn/" target="_blank">鄂ICP备2020015769号-1</a>',
   displayFooter: true,
+  pageInfo: ["Author", "Category", "Tag", "Original", "Word", "ReadingTime"],
 
-  plugins: {
+  markdown: {
+    align: true,
+    codeTabs: true,
+    gfm: true,
+    include: {
+      resolvePath: (file, cwd) => {
+        if (file.startsWith("@"))
+          return path.resolve(
+            __dirname,
+            "../snippets",
+            file.replace("@", "./"),
+          );
+
+        return path.resolve(cwd, file);
+      },
+    },
+    tasklist: true,
+  },
+
+ plugins: {
     blog: true,
+
+    copyright: {
+      author: "JavaGuide(javaguide.cn)",
+      license: "MIT",
+      triggerLength: 100,
+      maxLength: 700,
+      canonical: "https://javaguide.cn/",
+      global: true,
+    },
+
     feed: {
+      atom: true,
       json: true,
+      rss: true,
     },
-    mdEnhance: {
-      gfm: true,
+
+    icon: {
+      assets: "//at.alicdn.com/t/font_2410206_vuzkjonf4s9.css",
     },
-    searchPro: true,
+
+    search: {
+      isSearchable: (page) => page.path !== "/",
+      maxSuggestions: 10,
+    },
   },
 });
